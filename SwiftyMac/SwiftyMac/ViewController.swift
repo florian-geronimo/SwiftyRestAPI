4ea638fcb505b6db8398b33691ce45854ba4aff0
Daniel Lozano <danlozano@gmail.com>
2017-12-21 15:38:39 -0600

    added mac app

---
+++ SwiftyMac/SwiftyMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftyMac
+//
+//  Created by Daniel Lozano Valdés on 12/21/17.
+//  Copyright © 2017 icalialabs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
